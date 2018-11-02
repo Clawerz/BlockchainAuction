9d0e4a604b86672ca2153d876322f4e833f8642a
Carlos Ribeiro <carlosfiliperibeiro@ua.pt>
2018-11-02 14:51:06 +0000

    Novas classes, auction e bid

---
+++ BlockchainAuction/src/Auction/Auction.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Auction;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ *
+ * @author cfr
+ */
+public class Auction {
+    private String auctionName;
+    private int creatorID;
+    private int ID;
+    private int timeToFinish;
+    private boolean auctionFinished;
+    private int buyerID;
+    private ArrayList<Bid> bids = new ArrayList<Bid>();
+    
+    
+}
---
+++ BlockchainAuction/src/Auction/Bid.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Auction;
+
+import java.util.Date;
+
+/**
+ *
+ * @author cfr
+ */
+public class Bid {
+    private Date date;
+    private double value;
+    private int clientID;
+}
